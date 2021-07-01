619180b4b096b89e6971fd2f0c505b1062ef89f5
IndrajeetTech2020 <indrajeetpradhan1998@gmail.com>
2021-07-01 12:26:12 +0530

    first commit

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ EXPOSE 8501
 RUN pip install streamlit requests pandas
 COPY app.py /var/dashboard/app.py
 CMD streamlit run /var/dashboard/app.py
+
+
--- app.py
+++ app.py
@@ -85,6 +85,9 @@ final_df = None
 for INP_DATE in date_str:
     URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(DIST_ID, INP_DATE)
     response = requests.get(URL, headers=browser_header)
+    print(response)
+    print("URL : "+URL)
+    print(browser_header)
     if (response.ok) and ('centers' in json.loads(response.text)):
         resp_json = json.loads(response.text)['centers']
         if resp_json is not None:
